862a04c62728df4df0f8daa3722da96610faddf8
Askepios <alejandro.rojas.h@outlook.com>
2026-09-07 15:27:08 -0500

    compatibility with certain file types

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from pathlib import Path
+import shutil
 
-rutaOrden = Path("/home/askepios/AskepiosVault")
+ruteOrder = Path.home() / "AskepiosVault"
+ruteImages = Path.home() / "AskepiosVault/Images"
+ruteDocs = Path.home() / "AskepiosVault/Docs"
+ruteMusic = Path.home() / "AskepiosVault/Music"
 
-for i in rutaOrden.iterdir():
-    print(i.name)
+print("FILES FOUNDED:")
+print("______________________________")
+for i in ruteOrder.iterdir():
+    if i.is_file():
+        print(f"File: {i.name}")
+    else:
+        print(f"Directory: {i.name} ")
 
+for j in ruteOrder.iterdir():
+    if j.suffix.lower() == ".jpg" or j.suffix.lower() == ".png":
+        shutil.move(j, ruteImages)
+    elif j.suffix.lower() == ".pdf" or j.suffix.lower() == ".docx":
+        shutil.move(j, ruteDocs)
+    elif j.suffix.lower() == ".mp3":
+        shutil.move(j, ruteMusic)
+    else:
+        pass
 
+print("______________________________")
+print("Files moved succesfully...")
