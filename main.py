0369a069b85665179d9c611ad878c43b0be0fd39
Askepios <alejandro.rojas.h@outlook.com>
2026-09-07 19:09:40 -0500

    codigo ordenado y mas extenciones soportadas

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from pathlib import Path
 import shutil
 
+img = {".jpg", ".jpeg", ".png"}
+doc = {".pdf", ".docx", ".txt"}
+audio = {".mp3", ".wav", ".flac"}
+video = {".mp4", ".avi", ".aac", ".webm"}
+code = {".py", ".js", ".html", ".css"}
+compressed = {".zip", ".rar"}
+
 ruteOrder = Path.home() / "AskepiosVault"
 ruteImages = Path.home() / "AskepiosVault/Images"
 ruteDocs = Path.home() / "AskepiosVault/Docs"
 ruteMusic = Path.home() / "AskepiosVault/Music"
 ruteOther = Path.home() / "AskepiosVault/Other"
+ruteCompressed = Path.home() / "AskepiosVault/Compressed"
+ruteVideos = Path.home() / "AskepiosVault/Videos"
 
 print("FILES FOUNDED:")
 print("______________________________")
@@ -16,12 +25,17 @@ for i in ruteOrder.iterdir():
         print(f"Directory: {i.name} ")
 
 for j in ruteOrder.iterdir():
-    if j.suffix.lower() == ".jpg" or j.suffix.lower() == ".png" or j.suffix.lower() == ".jpeg":
+    extencion = j.suffix.lower()
+    if extencion in img:
         shutil.move(j, ruteImages)
-    elif j.suffix.lower() == ".pdf" or j.suffix.lower() == ".docx":
+    elif extencion in doc:
         shutil.move(j, ruteDocs)
-    elif j.suffix.lower() == ".mp3":
+    elif extencion in audio:
         shutil.move(j, ruteMusic)
+    elif extencion in compressed:
+        shutil.move(j, ruteCompressed)
+    elif extencion in video: 
+        shutil.move(j, ruteVideos)
     else:
         pass
         
